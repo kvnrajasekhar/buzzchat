221945f3b139bad4ff49ad4acbe8b8631ba38e1c
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2024-10-27 21:47:07 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line
+import { Button } from "@chakra-ui/react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+import VideoChat from "./Components/VideoChat";
+import ForgotPassword from "./Components/Authentication/ForgotPassword";
+/* eslint-disable no-unused-vars */
+import ResetPassword from "./Components/Authentication/ResetPassword";
+import Login from "./Components/Authentication/Login";
+import EmailVerify from "./Components/Authentication/EmailVerify";
+
+function App() {
+  return (
+    <div className="App">
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chat" component={ChatPage} />
+      <Route path="/videochat" component={VideoChat} />
+      <Route path="/forgotpassword" component={ForgotPassword} />
+      <Route
+        path="/resetForgotPassword/:userId/:token"
+        component={ResetPassword}
+      />
+      <Route path="/login" component={Login} />
+      <Route path="/user/:id/verify/:token" component={EmailVerify} />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Context/ChatProvider.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext, useEffect } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+// import { useEffect } from "react";
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [notification, setNotification] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const [chats, setChats] = useState([]);
+  const [aiMessage, setAiMessage] = useState("");
+  let history = useHistory();
+  // console.log(history);
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) {
+      {
+        history && history.push("/");
+      }
+    }
+  }, [history]);
+  return (
+    <ChatContext.Provider
+      value={{
+        user,
+        setUser,
+        selectedChat,
+        setSelectedChat,
+        chats,
+        setChats,
+        notification,
+        setNotification,
+        darkMode,
+        setDarkMode,
+        aiMessage,
+        setAiMessage,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
